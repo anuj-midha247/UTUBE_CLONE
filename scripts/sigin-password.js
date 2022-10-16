b9f735729e03cb1358d9dfc76360a37882a8c21f
Flixion <adityasingh200141@gmail.com>
2022-10-17 04:44:09 +0530

    login signup

--- scripts/sigin-password.js
+++ scripts/sigin-password.js
@@ -1,7 +1,9 @@
 document.querySelector("form").addEventListener("submit", myForm);
 
 var userData = JSON.parse(localStorage.getItem("userData")) || [];
-var name = localStorage.getItem("emailloop");
+var emailloop = localStorage.getItem("emailloop");
+var name = localStorage.getItem("name");
+let flag = false;
 console.log("userData:", userData);
 document.getElementById("userName").innerText = `Hi ${name} !`;
 
@@ -17,23 +19,29 @@ function myForm() {
     } else {
       document.getElementById("password").style.border = "";
       document.getElementById("error").innerText = "";
-      console.log(userData.password1);
+      console.log(el.password1);
       console.log(password);
-      if (el.password1 !== password && el.email !== emailloop) {
+      if (el.password1 === password && el.email === emailloop) {
         document.getElementById("password").style.border = "";
         document.getElementById("error").innerText = "";
-        document.getElementById("error").innerText =
-          "!Wrong password. Try again or click Forgot password to reset it.";
-        document.getElementById("password").style.border = "1px solid red";
-      } else if (el.password1 === password && el.email === emailloop) {
-        document.getElementById("password").style.border = "";
-        document.getElementById("error").innerText = "";
+        flag = true;
+        localStorage.setItem("login",true);
         alert("Signin successfull");
-        location.href = "./index.html";
       }
+    }
+    if (flag === true) {
+      location.href = "./index.html";
+    }
+    if (flag === false) {
+      document.getElementById("password").style.border = "";
+      document.getElementById("error").innerText = "";
+      document.getElementById("error").innerText =
+        "!Wrong password. Try again or click Forgot password to reset it.";
+      document.getElementById("password").style.border = "1px solid red";
     }
   });
 }
+
 document.getElementById("creat").addEventListener("click", goTo);
 
 function goTo() {
